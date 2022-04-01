a9a9386a4f2ea4351e0961fede82b4662d8bb724
Mac_Developer <araozangel842@gmail.com>
2022-04-01 16:18:01 +0300

    Dashboard and styles

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ChatContainer from '../components/ChatContainer';
+import { useStyles } from './styles/DashboardStyles';
+import TinderCard from 'react-tinder-card';
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	const characters = [
+		{ name: 'Jared Domn', url: 'https://i.imgur.com/Q9WPlWA.jpeg' },
+		{ name: 'Dinesh', url: 'https://i.imgur.com/OckVkRo.jpeg' },
+		{ name: 'Monica', url: 'https://i.imgur.com/wDmRJPc.jpeg' },
+		{ name: 'Chandler', url: 'https://i.imgur.com/H07Fxdh.jpeg' },
+		{ name: 'Ross', url: 'https://i.imgur.com/MWAcQRM.jpeg' },
+	];
+	const styles = useStyles();
+
+	const [lastDirection, setLastDirection] = useState();
+
+	const swiped = (direction, nameToDelete) => {
+		console.log('removing: ' + nameToDelete);
+		setLastDirection(direction);
+	};
+	// const onSwipe = (direction) => {
+	// 	console.log('onSwipe: ' + direction);
+	// };
+	const outOfFrame = (name) => {
+		console.log(name + ' left the screen');
+	};
+
+	return (
+		<div className={styles.dashboard}>
+			<ChatContainer />
+			<div className={styles.swiperContainer}>
+				<div className={styles.cardContainer}>
+					{characters.map((character) => (
+						<TinderCard
+							className={styles.swipe}
+							key={character.name}
+							onSwipe={(dir) => swiped(dir, character.name)}
+							// onSwipe={onSwipe}
+							onCardLeftScreen={() => outOfFrame(character.name)}
+						>
+							<div
+								style={{ backgroundImage: 'url(' + character.url + ')' }}
+								className={styles.card}
+							>
+								<h3 className={styles.title}>{character.name}</h3>
+							</div>
+						</TinderCard>
+					))}
+				</div>
+				<div className={styles.swipeInfo}>
+					{lastDirection ? <p>You swipe {lastDirection}</p> : <p />}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
---
+++ src/pages/styles/DashboardStyles.js
@@ -0,0 +1,52 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+	dashboard: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+	},
+	swiperContainer: {
+		width: '70%',
+		margin: '0 auto',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		height: '80vh',
+	},
+	cardContainer: {
+		width: '400px',
+		height: '550px',
+		position: 'relative',
+		backgroundColor: 'red',
+	},
+	swipe: {
+		position: 'absolute',
+	},
+	card: {
+		position: 'relative',
+		backgroundColor: '#fff',
+		width: '80vw',
+		maxWidth: '340px',
+		height: '300px',
+		boxShadow: '-1px 0 13px 1px rgba(0,0,0,0.12)',
+		borderRadius: '15px',
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+	},
+	cardContent: {
+		width: '100%',
+		height: '100%',
+	},
+	title: {
+		position: 'absolute',
+		bottom: 0,
+		margin: '10px',
+		color: '#fff',
+	},
+	swipeInfo: {
+		width: 'fit-content',
+		postion: 'absolute',
+		padding: '10px',
+	},
+});
